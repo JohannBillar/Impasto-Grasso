f590ae950ee8926d7ecf0390b2842eab188be99f
JohannBillar <jwbillar@gmail.com>
2016-04-26 10:35:34 -0700

    Add compass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp       = require('gulp'),
     gutil      = require('gulp-util'),
     browserify = require('gulp-browserify'),
+    compass    = require('gulp-compass'),
     concat     = require('gulp-concat');
 
 gulp.task('log', function() {
@@ -12,9 +13,23 @@ var jsSources = [
   'components/scripts/form-validation.js'
 ];
 
+var sassSources = ['components/sass-stylesheets/main.scss'];
+
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass-stylesheets',
+      image: 'builds/development/images',
+      font: 'builds/development/fonts',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'));
 });
