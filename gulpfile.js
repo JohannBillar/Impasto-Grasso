5b6b1f6b02ad6f16e0aebd56c34927bd9f033204
JohannBillar <jwbillar@gmail.com>
2016-04-26 13:26:34 -0700

    Add live reloading with gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp       = require('gulp'),
     gutil      = require('gulp-util'),
     browserify = require('gulp-browserify'),
     compass    = require('gulp-compass'),
+    connect    = require('gulp-connect'),
     concat     = require('gulp-concat');
 
 gulp.task('log', function() {
@@ -19,7 +20,8 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'));
+    .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload());
 });
 
 gulp.task('compass', function() {
@@ -31,12 +33,20 @@ gulp.task('compass', function() {
       style: 'expanded'
     }))
     .on('error', gutil.log)
-    .pipe(gulp.dest('builds/development/css'));
+    .pipe(gulp.dest('builds/development/css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass-stylesheets/**/*', ['compass']);
 });
 
-gulp.task('default', ['js', 'compass', 'watch']);
+gulp.task('default', ['js', 'compass', 'connect', 'watch']);
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'builds/development',
+    livereload: true
+  })
+} );
