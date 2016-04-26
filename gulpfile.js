b7149f801abc6f4335e9f3e79ebe56afd5d7cc20
JohannBillar <jwbillar@gmail.com>
2016-04-26 13:04:40 -0700

    Add watch task default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -34,4 +34,9 @@ gulp.task('compass', function() {
     .pipe(gulp.dest('builds/development/css'));
 });
 
-gulp.task('default', ['js', 'compass']);
+gulp.task('watch', function(){
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass-stylesheets/**/*', ['compass']);
+});
+
+gulp.task('default', ['js', 'compass', 'watch']);
